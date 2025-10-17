fbd529df228176f883588f3fbab62fa3c1c563e3
Polo64190 <paul.d00503@anaten.fr>
2025-10-17 09:37:22 +0200

    Remise à niveau

--- Assets/Guillaume/Script/Trigger.cs
+++ Assets/Guillaume/Script/Trigger.cs
@@ -9,6 +9,21 @@ public class Trigger : MonoBehaviour
     [SerializeField] private PlayerMoovement player;
     [SerializeField] GameObject CanvasObject;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        CanvasObject.SetActive(true);
+
+        if (Input.GetKeyDown(KeyCode.E) && other.tag == "Player")
+        {
+
+            Debug.Log("SceneLancement)");
+            SceneManager.LoadScene("PaulScene");
+            CanvasObject.SetActive(false);
+        }
+
+
+
+    }
 
     private void OnTriggerStay(Collider other)
     {
